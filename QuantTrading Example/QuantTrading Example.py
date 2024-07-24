9da6be053bd2ef31e929aae58d5b52db47917525
Jongjh <72982650+Jongjh@users.noreply.github.com>
2024-07-24 21:59:59 +0800

    Add files via upload

---
+++ QuantTrading Example/QuantTrading Example.py
@@ -0,0 +1,136 @@
+"""
+Created on Sat Jun 29 11:41:47 2024
+
+@author: Jun Hui
+"""
+
+# Import necesary libraries
+import yfinance as yf
+import numpy as np
+import pandas as pd
+
+# Download historical data for required stocks
+tickers = ["AMZN", "MSFT", "GOOG", "META", "TSLA", "COIN"]
+ohlcv_data = {}
+
+# looping over tickers and storing OHLCV dataframe in dictionary
+for ticker in tickers:
+    temp = yf.download(ticker,period='1mo',interval='5m')
+    temp.dropna(how="any",inplace=True)
+    ohlcv_data[ticker] = temp
+
+#Technical Indicators
+def MACD(DF, a=12 ,b=26, c=9):
+    """function to calculate MACD
+       typical values a(fast moving average) = 12; 
+                      b(slow moving average) =26; 
+                      c(signal line ma window) =9"""
+    df = DF.copy()
+    df["ma_fast"] = df["Adj Close"].ewm(span=a, min_periods=a).mean()
+    df["ma_slow"] = df["Adj Close"].ewm(span=b, min_periods=b).mean()
+    df["macd"] = df["ma_fast"] - df["ma_slow"]
+    df["signal"] = df["macd"].ewm(span=c, min_periods=c).mean()
+    return df.loc[:,["macd","signal"]]
+
+def Boll_Band(DF, n=14):
+    "function to calculate Bollinger Band"
+    df = DF.copy()
+    df["MB"] = df["Adj Close"].rolling(n).mean()
+    df["UB"] = df["MB"] + 2*df["Adj Close"].rolling(n).std(ddof=0)
+    df["LB"] = df["MB"] - 2*df["Adj Close"].rolling(n).std(ddof=0)
+    df["BB_Width"] = df["UB"] - df["LB"]
+    return df[["MB","UB","LB","BB_Width"]]
+
+def RSI(DF, n=14):
+    df = DF.copy()
+    df["change"] = df["Adj Close"] - df["Adj Close"].shift(1)
+    df["gain"] = np.where(df["change"]>=0,df["change"],0)
+    df["loss"] = np.where(df["change"]<0,-1*df["change"],0)
+    df["avgGain"] = df["gain"].ewm(alpha=1/n, min_periods=n).mean()
+    df["avgLoss"] = df["loss"].ewm(alpha=1/n, min_periods=n).mean()
+    df["rs"] = df["avgGain"]/df["avgLoss"]
+    df["rsi"] = 100 - (100/(1+df["rs"]))
+    return df["rsi"]
+
+def ATR(DF, n=14):
+    "function to calculate True Range and Average True Range"
+    df = DF.copy()
+    df["H-L"] = df["High"] - df["Low"]
+    df["H-PC"] = abs(df["High"] - df["Adj Close"].shift(1))
+    df["L-PC"] = abs(df["Low"] - df["Adj Close"].shift(1))
+    df["TR"] = df[["H-L","H-PC","L-PC"]].max(axis=1, skipna=False)
+    df["ATR"] = df["TR"].ewm(com=n, min_periods=n).mean()
+    return df["ATR"]
+
+def ADX(DF, n=20):
+    "function to calculate ADX"
+    df = DF.copy()
+    df["ATR"] = ATR(DF, n)
+    df["upmove"] = df["High"] - df["High"].shift(1)
+    df["downmove"] = df["Low"].shift(1) - df["Low"]
+    df["+dm"] = np.where((df["upmove"]>df["downmove"]) & (df["upmove"] >0), df["upmove"], 0)
+    df["-dm"] = np.where((df["downmove"]>df["upmove"]) & (df["downmove"] >0), df["downmove"], 0)
+    df["+di"] = 100 * (df["+dm"]/df["ATR"]).ewm(alpha=1/n, min_periods=n).mean()
+    df["-di"] = 100 * (df["-dm"]/df["ATR"]).ewm(alpha=1/n, min_periods=n).mean()
+    df["ADX"] = 100* abs((df["+di"] - df["-di"])/(df["+di"] + df["-di"])).ewm(alpha=1/n, min_periods=n).mean()
+    return df["ADX"]
+
+#Adding indicators into DF
+for ticker in ohlcv_data:
+    ohlcv_data[ticker][["MB","UB","LB","BB_Width"]] = Boll_Band(ohlcv_data[ticker])
+    
+for ticker in ohlcv_data:
+    ohlcv_data[ticker][["MACD","SIGNAL"]] = MACD(ohlcv_data[ticker], a=12 ,b=26, c=9)  
+    
+for ticker in ohlcv_data:
+    ohlcv_data[ticker]["RSI"] = RSI(ohlcv_data[ticker])
+
+for ticker in ohlcv_data:
+    ohlcv_data[ticker]["ADX"] = ADX(ohlcv_data[ticker],20)
+    
+#Adding measurements  
+def CAGR(DF):
+    df = DF.copy()
+    df["return"] = DF["Adj Close"].pct_change()
+    df["cum_return"] = (1 + df["return"]).cumprod()
+    n = len(df)/252
+    CAGR = (df["cum_return"][-1])**(1/n) - 1
+    return CAGR
+    
+def volatility(DF):
+    df = DF.copy()
+    df["daily_ret"] = DF["Adj Close"].pct_change()
+    vol = df["daily_ret"].std() * np.sqrt(252) #annualize
+    return vol
+
+def Sharpe_ratio(DF,rf=0.03):
+    df = DF.copy()
+    return (CAGR(df) - rf) / volatility(df) 
+    
+def sortino(DF, rf=0.03):
+    df = DF.copy()    
+    df["return"] = df["Adj Close"].pct_change()
+    neg_return = np.where(df["return"]>0,0,df["return"])    
+    neg_vol = pd.Series(neg_return[neg_return != 0]).std() * np.sqrt(252)
+    return (CAGR(df) - rf) / neg_vol 
+
+def max_dd(DF):
+    df = DF.copy()
+    df["return"] = df["Adj Close"].pct_change()
+    df["cum_return"] = (1+df["return"]).cumprod()
+    df["cum_roll_max"] =  df["cum_return"].cummax()
+    df["drawdown"] =  df["cum_roll_max"]-df["cum_return"]
+    return (df["drawdown"]/df["cum_roll_max"]).max()
+
+def calmar(DF): #measure of risk adjusted return
+    df = DF.copy()
+    return CAGR(df)/max_dd(DF)
+    
+#Print results
+for ticker in ohlcv_data:
+    print("CAGR of {} = {}".format(ticker,CAGR(ohlcv_data[ticker])))
+    print("vol for {} = {}".format(ticker,volatility(ohlcv_data[ticker])))
+    print("Sharpe for {} = {}".format(ticker,Sharpe_ratio(ohlcv_data[ticker])))
+    print("Sortino for {} = {}".format(ticker,sortino(ohlcv_data[ticker])))
+    print("Max Drawdown for {} = {}".format(ticker,max_dd(ohlcv_data[ticker])))
+    print("calmar ratio for {} = {}".format(ticker,calmar(ohlcv_data[ticker])))
